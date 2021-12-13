b689a0887227707388a76902b396002d17d2f2bb
Clem <clement.bolin@epitech.eu>
2021-12-13 14:10:36 +0100

    add(feat/mqtt_service): mqtt client

--- src/mqtt/mqttapp.controller.ts
+++ src/mqtt/mqttapp.controller.ts
@@ -1,4 +1,11 @@
 import { Controller } from '@nestjs/common';
+import { Ctx, MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
-export class MqttAppController {}
+export class MqttAppController {
+  @MessagePattern('esp32/watt')
+  getNotificationWatt(@Payload() data: number, @Ctx() context: any) {
+    console.log(data);
+    console.log(context);
+  }
+}
--- src/mqtt/mqttapp.module.ts
+++ src/mqtt/mqttapp.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { MqttAppController } from './mqttapp.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'MQTT_SERVICE',
+        transport: Transport.MQTT,
+        options: {
+          url: 'mqtt://localhost:1883',
+        },
+      },
+    ]),
+  ],
   controllers: [MqttAppController],
 })
 export class MqttAppModule {}
