e238c2bcd91c29cf88a4b9ebe726869510b7dc7e
Clem <clement.bolin@epitech.eu>
2021-12-14 16:33:05 +0100

    add(feat/mqtt_service): create and fill table

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,8 +1,33 @@
---- Create User table
-CREATE TABLE IF NOT EXISTS "user" (
+--- Creation of user table
+CREATE TABLE IF users (
   id SERIAL NOT NULL,
+  uuid varchar(36) NOT NULL,
   username varchar(255) NOT NULL,
   password varchar(255) NOT NULL,
   email varchar(255) NOT NULL,
+  firstname varchar(255) NOT NULL,
+  lastname varchar(255) NOT NULL,
+  birthdate date NOT NULL,
   PRIMARY KEY (id)
+);
+
+--- Creation of user_role table
+CREATE TABLE user_role (
+  id SERIAL NOT NULL,
+  role_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) 
+      REFERENCES users (id)
+);
+
+--- Creatation of data esp32 for each user table
+CREATE TABLE data_esp32 (
+  id SERIAL NOT NULL,
+  user_id INTEGER NOT NULL,
+  data_esp32 varchar(255) NOT NULL,
+  date_esp32 TIMESTAMP NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) 
+      REFERENCES users (id)
 );
