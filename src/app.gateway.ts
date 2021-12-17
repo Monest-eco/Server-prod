f2c2725c7eaa3cecc95e9825e4b0650e0e32bc59
Richard Dépierre <richard.habimana@epitech.eu>
2021-12-17 16:45:36 +0100

    added websockets on the server side

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -4,30 +4,46 @@ import {
   WebSocketServer,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  OnGatewayInit,
+  ConnectedSocket,
 } from '@nestjs/websockets';
 import { Logger } from '@nestjs/common';
 import { Socket, Server } from 'socket.io';
+import { Message } from './@types/websockets';
 
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
 })
-export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  @WebSocketServer() server: Server;
-
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server;
   private logger: Logger = new Logger('AppGateway');
 
-  @SubscribeMessage('msgToServer')
-  handleMessage(client: Socket, payload: string): void {
-    this.server.emit('msgToClient', payload);
+  afterInit(server: Server) {
+    this.logger.log('Initialized !');
   }
 
-  async handleDisconnect(client: Socket) {
+  handleConnection(@ConnectedSocket() client: Socket, ...args: any[]) {
+    this.logger.log('Client connected:', client.id);
+  }
+  handleDisconnect(@ConnectedSocket() client: Socket) {
     this.logger.log(`Client disconnected: ${client.id}`);
   }
 
-  async handleConnection(client: Socket, ...args: any[]) {
-    this.logger.log(`Client connected: ${client.id}`);
+  @SubscribeMessage('data')
+  handleMessage(@ConnectedSocket() client: Socket, message: Message): string {
+    this.server
+      .to(message.id)
+      .emit('Websocket-message: watt:', message.watt, '-date:', message.date);
+    return 'Data sent';
+  }
+  @SubscribeMessage('join')
+  handleJoin(@ConnectedSocket() client: Socket, id: string): string {
+    client.join(id);
+    client.emit('id', id);
+    return 'Connected';
   }
 }
